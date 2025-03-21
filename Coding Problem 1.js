34d64bb20d786379263dbfdfa3e56e3217fac767
Bretny Bal-ut <abonbretny@gmail.com>
2025-03-21 21:06:21 +0800

    userProfile.js

--- Coding Problem 1.js
+++
@@ -1,9 +0,0 @@
-let fullName = prompt(("Enter your full  name"));
-let Age = parseInt(prompt("Enter your age"));
-let FavNum = parseInt(prompt("Enter your favorite number"));
-let FavColor = prompt(("Enter your favorite color"));
-
-console.log(fullName);
-console.log(Age);
-console.log(FavNum);
-console.log(FavColor);
---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let FullName = prompt("Enter your full name");
+let Age = prompt("How old are you");
+let FavNum = prompt("Enter your favorite_number");
+let FavColor = prompt("Enter your favorite color");
+
+console.log("Full name:", FullName);
+console.log("Age:", Age);
+console.log("Favorite Number:", FavNum);
+console.log("Favorite Color:", FavColor);
