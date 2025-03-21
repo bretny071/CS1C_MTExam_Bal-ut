ef6a95d75d7c6428d12dfb78f99e90f91612ae69
Bretny Bal-ut <abonbretny@gmail.com>
2025-03-21 21:25:01 +0800

    favoriteNumberCalculations.js

---
+++ favoriteNumberCalculations.js
@@ -0,0 +1,14 @@
+let FavoriteNumber = 07;
+let correct = false;
+
+while (!correct) {
+    var guessNumber = parseInt(prompt("Can you guess my favorite number?"), 10);
+
+    if (guessNumber < FavoriteNumber) {
+        console.log("Too Low!");
+    } else if (guessNumber > FavoriteNumber) {
+        console.log("Too High!");
+    } else {
+        console.log("Correct!");
+    }
+}
